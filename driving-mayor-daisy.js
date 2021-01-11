79e7e2fb616e6410573f29700b8ae3cdbc92722d
Ibrahim Khalilov <ibrahim.khalilov@AB02380.corp.applyboard.com>
2021-01-11 10:01:08 -0500

    solution

---
+++ driving-mayor-daisy.js
@@ -0,0 +1,9 @@
+const carPassing = function(cars, speed) {
+  let car = {
+    time: Date.now(),
+    speed: speed
+  };
+  cars.push(car);
+  return cars;
+}
+console.log(carPassing([], 50))
---
+++ we-are-rooting-for-you.js
@@ -0,0 +1,43 @@
+function judgeVegetable(vegetables, metric) {
+
+  if (metric === 'redness') {
+
+    const [Biggest] = vegetables.sort((a, b) => b.redness - a.redness)
+
+    return Biggest.submitter;
+
+  } else if (metric === 'plumpness') {
+
+    const [Plumpness] = vegetables.sort((a, b) => b.plumpness - a.plumpness)
+
+    return Plumpness.submitter;
+
+  } else {
+
+    return undefined;
+
+  }
+
+}
+
+
+const vegetables = [
+  {
+    submitter: 'Old Man Franklin',
+    redness: 10,
+    plumpness: 5
+  },
+  {
+    submitter: 'Sally Tomato-Grower',
+    redness: 2,
+    plumpness: 8
+  },
+  {
+    submitter: 'Hamid Hamidson',
+    redness: 4,
+    plumpness: 3
+  }
+];
+
+const metric = 'redness'
+console.log(judgeVegetable(vegetables, metric));
