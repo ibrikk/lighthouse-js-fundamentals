d9d744e6209c1a02bb0e631d0b43db404fd1c092
Ibrahim Khalilov <ibrahim.khalilov@AB02380.corp.applyboard.com>
2021-01-12 12:31:55 -0500

    solution

--- last-index-of.js
+++ last-index-of.js
@@ -1,15 +1,14 @@
 function lastIndexOf(arr, val) {
 
-if (arr.length === 0) {
-  return -1
-}
-  for (let i = arr.length-1; i >= 0; i--) {
+  if (arr.length === 0) {
+    return -1
+  }
+  for (let i = arr.length - 1; i >= 0; i--) {
     if (arr[i] === val) {
       return i
-    } else {
-      return -1
     }
-  } 
+  }
+  return -1
 }
 
 console.log(lastIndexOf([0, 1, 4, 1, 2], 1));
@@ -17,3 +16,4 @@ console.log(lastIndexOf([0, 1, 4, 1, 2], 2));
 console.log(lastIndexOf([0, 1, 4, 1, 2], 3));
 console.log(lastIndexOf([5, 5, 5], 5));
 console.log(lastIndexOf([], 3));
+
