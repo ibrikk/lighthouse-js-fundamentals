5bda7800e7fa28f3eca6db1430dec0b1ae7a31fb
Ibrahim Khalilov <ibrahim.khalilov@AB02380.corp.applyboard.com>
2021-01-18 16:17:21 -0500

    solution

--- katas/vowels.js
+++ katas/vowels.js
@@ -1,11 +1,16 @@
+// In this exercise, we will be counting the number of vowels that appear in a given string. 
+// For this exercise, consider the following to be vowels: a, e, i, o, and u.
+// Create a function named numberOfVowels that will receive a string and return the number of vowels 
+// in that string.
 const numberOfVowels = function (data) {
-  let num = [];
+  let num = 0;
   for (let i = 0; i < data.length; i++) {
     if (data[i] === 'a' || data[i] === 'e' || data[i] === 'i' || data[i] === 'o' || data[i] === 'u') {
-      return num.push(data[i]);
+       // num.push(data[i]);
+       num ++;
     } 
   }
-  return num.length;
+  return num;
 };
 
 console.log(numberOfVowels("orange"));
